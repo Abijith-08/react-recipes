f279226ea1327bafd49371f53049b5b157f25fe4
esteves98 <martim.ramalho@bjss.com>
2021-11-26 14:35:11 +0000

    message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
 const APP_ID = "1475a000";
 const APP_KEY = "628f8482c4d55cdd67efa07af2806f71";
 const [recipes, setRecipes] = useState([]);
+const [calories, setCalories] = useState([]);
 const [search, setSearch] = useState("");
 const [query, setQuery] = useState("");
 useEffect(() => {
@@ -37,11 +38,27 @@ return (
 			Search
 		</button>
 	</form>
+
+	<form className="calories-filter" onSubmit={getSearch} >
+		<input className="filter-bar" type="text" value={search}
+			onChange={updateSearch} />
+		<button className="filter-button" type="submit" >
+			Search
+		</button>
+	</form>
+
 	<div className="recipes">
 		{recipes.map(recipe => (
 		<Recipe
 			key={recipe.recipe.label}
 			title={recipe.recipe.label}
+
+				/*
+				take calories as input from user
+				if calories <= input then show
+			 	input = 100 million by default 
+				 */
+
 			calories={recipe.recipe.calories}
 			image={recipe.recipe.image}
 			ingredients={recipe.recipe.ingredients}
